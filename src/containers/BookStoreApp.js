96bcd080ed19d5f4a2a3bf1d342742cebde02bb7
Irinkes <irinkes@gmail.com>
2018-03-07 17:03:43 +0300

    BookItem component added

--- src/containers/BookStoreApp.js
+++ src/containers/BookStoreApp.js
@@ -1,11 +1,34 @@
-import React from 'react';
+// import React from 'react';
+//
+// export default class BookStoreApp extends React.Component {
+//   render() {
+//     return <div>BookStoreApp</div>;
+//   }
+// }
 
-export default class BookStoreApp extends React.Component {
-	render(){
-		return(
-			<div>
-				BookStoreApp
-			</div>
-		);
-	}
-}
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Booklist from '../components/BooksList';
+import Home from '../components/Home';
+
+const BookStoreApp = () => (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/books">Books</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/books" component={Booklist} />
+    </div>
+  </Router>
+);
+
+export default BookStoreApp;
