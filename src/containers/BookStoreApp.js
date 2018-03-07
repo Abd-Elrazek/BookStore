b3182a0dfc2b5f59d78c93e72fbc875f816a7d65
Yuliya Ayubova <ayubova.yuliya@gmail.com>
2018-03-07 15:23:27 +0300

    add fetching

--- src/containers/BookStoreApp.js
+++ src/containers/BookStoreApp.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import searchBooks from '../utils/fetchApi';
 
-export default class BookStoreApp extends React.Component {
-	render(){
-		return(
-			<div>
-				BookStoreApp
-			</div>
-		);
-	}
+export default class Booklist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+    };
+  }
+
+  componentDidMount() {
+    searchBooks(query, queryParams)
+      .then(response => response.json())
+      .then(data => this.storeBooks(data))
+      .catch(error => console.log(error));
+  }
+  storeBooks = data => {
+    const books = data.items.map(
+      ({
+        id,
+        volumeInfo: { title },
+        volumeInfo: { subtitle },
+        volumeInfo: { authors },
+        volumeInfo: { publisher },
+        volumeInfo: { publishedDate },
+        volumeInfo: { description },
+        volumeInfo: { pageCount },
+        volumeInfo: { imageLinks: { thumbnail: imageLink } },
+      }) => {
+        return {
+          id,
+          title,
+          subtitle,
+          authors,
+          publisher,
+          publishedDate,
+          description,
+          pageCount,
+          imageLink,
+        };
+      },
+    );
+    this.setState({ books });
+  };
+
+  render() {
+            <BooksList {...book} />
+          
 }
+};
